c8ee206b72ce70299035104b82781cde3481e9f0
arushsaxena <36010578+arushsaxena@users.noreply.github.com>
2020-04-02 05:11:48 +0530

    Update Trie-Insert_Search.cpp

--- Trie-Insert_Search.cpp
+++ Trie-Insert_Search.cpp
@@ -1,74 +1,59 @@
-#include<iostream>
 using namespace std;
 #include<bits/stdc++.h>
-struct TrieNode
-{
- map <char,TrieNode*> mp;
-	bool isend;
+struct TrieNode{
+    unordered_map <char,TrieNode*> mp;
+    bool isendofword;
 };
 TrieNode* newnode(void)
 {
-	TrieNode* temp =new TrieNode;
-	temp->isend=false;
-	return temp;
+    TrieNode* temp = new TrieNode;
+    temp->isendofword = false;
+    return temp;
 }
-void insert(TrieNode*& temp,const string& str)
+void insert(TrieNode* root,string str)
 {
-	if(temp==nullptr)
-	temp=newnode();
-	
-	TrieNode* temp1=temp;
-	for(int i=0;str[i]!='\0';i++)
-	{
-		char x=str[i];
-		if(temp1->mp.find(x)==temp1->mp.end())
-		{
-			temp1->mp[x]=newnode();
-		}
-		temp1=temp1->mp[x];
-
-	}
-	temp1->isend=true;
-
-}
-void search(TrieNode* temp,const string& str)
-{
-    if(temp==nullptr)
+    if(root==nullptr)
+    root = newnode();
+    TrieNode* temp = root;
+    for(int i=0;i<str.size();i++)
     {
-        cout<<"NO"<<endl;
-        return;
+        if(temp->mp[str[i]]==nullptr)
+        {
+            temp->mp[str[i]]=newnode();
+            
+        }
+        temp = temp->mp[str[i]];
     }
-    TrieNode* temp1=temp;
-    for(int i=0;str[i]!='\0';i++)
-    {
-        temp1=temp1->mp[str[i]];
-        if(temp1==nullptr)
-        {
-            cout<<"NO"<<endl;
-            
-        }
-       
-    }
-     if(temp1->isend)
-        {
-            cout<<"YES"<<endl;
-            
-        }
-        else
-        {
-            cout<<"NO"<<endl;
-            
-        }
+    temp -> isendofword = true;
 }
-void delete (TrieNode* temp;const string& str)
+void search(TrieNode* root, string str)
 {
+    TrieNode* temp = root;
+    for(int i = 0;i<str.size();i++)
+    {
+        temp = temp->mp[str[i]];
+        if(temp==nullptr)
+        {
+            cout<<"NOT Found"<<endl;
+            return;
+        }
+    }
+    if(temp->isendofword)
+    {
+        cout<<"Found"<<endl;
+    }
+    else
+    {
+        cout<<"Not found"<<endl;
+    }
     
 }
 int main()
 {
-	TrieNode* root=nullptr;
-	insert(root,"hello");
-	insert(root,"arush");
-	search(root,"Hello2");
-
+    TrieNode* root = newnode();
+    insert(root,"hello");
+    insert(root,"hello1");
+    search(root,"hello");
+    search(root,"hello2");
+    return 0;
 }
